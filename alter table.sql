c3cf634c77bdfedce447efa69eba78e9ddd6ad10
Mayank <mayankrahani@gmail.com>
2026-08-23 19:27:30 +0530

    Update alter table.sql

--- alter table.sql
+++ alter table.sql
@@ -1,3 +1,7 @@
+-- EMP1 TABLE
+-- This table stores basic employee information.
+-- Important concepts: CREATE TABLE, column names, datatypes,
+-- NUMBER, VARCHAR2, CHAR, DATE.
 CREATE TABLE emp1
 (
     Emp_ID       NUMBER(5) ,
